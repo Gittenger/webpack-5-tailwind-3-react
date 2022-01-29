81071578d0d1c07b545f9b690d54ed5014120166
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-29 03:18:40 -0500

    Tailwind 3 working

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from 'react'
+import Github from './github.svg?react'
+import Bike from './bike.jpeg'
+import AngularSrc from './angular.svg'
 
 const HelloWorld = () => {
-  return (
-      <h3>Hello World</h3>
-  );
-};
+	return (
+		<div>
+			<div className="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+				<div className="sm:flex sm:items-center px-6 py-4">
+					<img
+						className="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0"
+						src="https://avatars.githubusercontent.com/u/10122431?s=400&v=4"
+						alt=""
+					/>
+					<div className="text-center sm:text-left sm:flex-grow">
+						<div className="mb-4">
+							<p className="text-xl leading-tight">John Doe</p>
+							<p className="text-sm leading-tight text-grey-dark">
+								Contributor at somerepo
+							</p>
+						</div>
+						<div>
+							<button className="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple-500 text-purple-500 hover:bg-purple hover:text-blue-400">
+								Message
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<img className="w-[40px] h-[40px]" src={AngularSrc} alt="" />
+			<img className="w-24" src={Bike} alt="" />
+			<Github />
+		</div>
+	)
+}
 
-export default HelloWorld;
-
+export default HelloWorld
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'build'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+	},
+	devServer: {
+		port: 3030,
 	},
 	resolve: {
 		modules: [path.join(__dirname, 'src'), 'node_modules'],
 		alias: {
-			react: path.join(__dirname, 'node_modules', 'react')
-		}
+			react: path.join(__dirname, 'node_modules', 'react'),
+		},
 	},
 	module: {
 		rules: [
@@ -18,26 +23,51 @@ module.exports = {
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader'
-				}
+					loader: 'babel-loader',
+				},
 			},
 			{
 				test: /\.css$/,
+				exclude: /node_modules/,
 				use: [
+					MiniCssExtractPlugin.loader,
 					{
-						loader: 'style-loader',
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						},
 					},
 					{
-						loader: 'css-loader'
-					}
-				]
-			}
-		]
+						loader: 'postcss-loader',
+					},
+				],
+			},
+			{
+				test: /\.svg$/i,
+				oneOf: [
+					{
+						issuer: /\.[jt]sx?$/,
+						resourceQuery: /react/,
+						use: ['@svgr/webpack'],
+					},
+					{
+						type: 'asset/resource',
+					},
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|ico)$/i,
+				type: 'asset/resource',
+			},
+		],
 	},
 	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'index.css',
+			chunkFilename: 'index.css',
+		}),
 		new HtmlWebPackPlugin({
-			template: './src/index.html'
-		})
-	]
-
+			template: './src/index.html',
+		}),
+	],
 }
