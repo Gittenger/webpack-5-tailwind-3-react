19244e53efc8bc320529453dc0f6f90d4a98f45a
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-02 17:38:52 -0500

    router v6 private route

--- src/components/Admin/AdminRoute.component.jsx
+++ src/components/Admin/AdminRoute.component.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Route, useNavigate } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 import auth from '../../utils/auth.js';
 
 const { checkAuthToken } = auth;
 
-const AdminRoute = ({ children, component: Component, ...rest }) => {
-  const navigate = useNavigate();
-
-  return checkAuthToken() && checkAuthToken().user.role === 'admin'
-    ? children
-    : navigate('/login', { state: { from: props.location } });
+const AdminRoute = ({ component: Component, ...rest }) => {
+  return checkAuthToken() && checkAuthToken().user.role === 'admin' ? (
+    <Component {...rest} />
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default AdminRoute;
