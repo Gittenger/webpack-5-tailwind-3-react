f57c9f485cfa610a7083b78ad9058808b4d706e4
John Pittenger <98927080+TheCodeCleric@users.noreply.github.com>
2022-02-02 23:41:04 -0500

    new format

--- src/assets/img/img-index.js
+++ src/assets/img/img-index.js
@@ -1,7 +1,21 @@
-import ImgOne from './img-1.jpeg'
-import ImgTwo from './img-2.jpeg'
-import ImgTnree from './img-3.jpeg'
+import EmailImg from './icons/email.svg?react'
+import FacebookImg from './icons/facebook.svg?react'
+import InstagramImg from './icons/instagram.svg?react'
+import ImgOne from './img/img-1.jpeg'
+import ImgTwo from './img/img-2.jpeg'
+import ImgTnree from './img/img-3.jpeg'
 
-export const LocalImages = [ImgOne, ImgTwo, ImgTnree]
+export const images = {
+  img: {
+    ImgOne,
+    ImgTwo,
+    ImgTnree,
+  },
+  icons: {
+    EmailImg,
+    FacebookImg,
+    InstagramImg,
+  },
+}
 
-export default LocalImages
+export default images
